7284889cb18e516269c0c6e4ee849d045e542af5
Alekseypavlov14 <aleshapavlov14@gmail.com>
2022-07-31 19:55:08 +0300

    init

---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
+import { ProjectComponent } from './components/project/project.component';
+import { ButtonComponent } from './components/button/button.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomePageComponent,
+    LoginPageComponent,
+    ProjectComponent,
+    ButtonComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
